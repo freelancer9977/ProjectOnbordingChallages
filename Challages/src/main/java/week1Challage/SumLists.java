da485ac7aa648dac3ca534bb551795d881ed9413
freelancer9977 <masp3836@colorado.edu>
2022-05-16 21:55:49 -0600

    start adding code

---
+++ Challages/src/main/java/week1Challage/SumLists.java
@@ -0,0 +1,14 @@
+package week1Challage;
+
+/*
+* Sum Lists: You have two numbers represented by a linked list, where each node contains a single
+* digit. The digits are stored in reverse order, such that the 1 's digit is at the head of the list. Write a
+* function that adds the two numbers and returns the sum as a linked list.
+* */
+
+public class SumLists {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
