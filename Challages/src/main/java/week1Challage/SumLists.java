f1d6fa3ec5f29c372b86c18d583f77375a6b5cc2
freelancer9977 <masp3836@colorado.edu>
2022-05-17 16:45:12 -0600

    sum list done

--- Challages/src/main/java/week1Challage/SumLists.java
+++ Challages/src/main/java/week1Challage/SumLists.java
@@ -6,9 +6,40 @@ package week1Challage;
 * function that adds the two numbers and returns the sum as a linked list.
 * */
 
+import java.util.LinkedList;
+
 public class SumLists {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        LinkedList<Integer> list2 = new LinkedList<Integer>();
+        list.add(6);
+        list.add(0);
+        list.add(1);
+        list2.add(5);
+        list2.add(9);
+        list2.add(2);
+        LinkedList<Integer> sum = Sum(list,list2);
+        System.out.println(sum);
+    }
+
+    public static LinkedList<Integer> Sum(LinkedList<Integer> list1,LinkedList<Integer> list2){
+        Double num1 = 0.0,num2 = 0.0;
+        Integer x,y,sum;
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        for (int i = 0; i< list1.size(); i++){
+            num1 = num1 + (list1.get(i)*(Math.pow(10,i)));
+        }
+        for (int i = 0; i< list2.size(); i++) {
+            num2 = num2 + (list2.get(i) * (Math.pow(10, i)));
+        }
+        x = (Integer) num1.intValue();
+        y = (Integer) num2.intValue();
+        sum = x+y;
+        while (sum>0){
+            list.add(sum%10);
+            sum = sum/10;
+        }
+        return list;
     }
 
 }
